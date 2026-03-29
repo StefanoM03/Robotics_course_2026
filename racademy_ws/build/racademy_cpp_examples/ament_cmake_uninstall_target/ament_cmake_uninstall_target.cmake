3a34b7c2261f7d10b44ddc7d1dc5317cf6b0e408
StefanoM03 <s.milantoni03@gmail.com>
2026-03-29 12:26:47 +0200

    update

--- racademy_ws/build/racademy_cpp_examples/ament_cmake_uninstall_target/ament_cmake_uninstall_target.cmake
+++ racademy_ws/build/racademy_cpp_examples/ament_cmake_uninstall_target/ament_cmake_uninstall_target.cmake
@@ -55,3 +55,6 @@ endforeach()
 message(STATUS "Execute custom uninstall script")
 
 # begin of custom uninstall code
+
+# uninstall files installed using the symlink install functions
+include("/github/racademy_ws/build/racademy_cpp_examples/ament_cmake_symlink_install/ament_cmake_symlink_install_uninstall_script.cmake")
